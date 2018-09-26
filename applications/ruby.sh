ea9bafa6af949070f9e962c233ee81e7084be2d1
xiewulong <xiewulong@vip.qq.com>
2018-09-26 14:40:55 +0800

    deploy

--- applications/ruby.sh
+++ applications/ruby.sh
@@ -67,9 +67,9 @@ install_ruby() {
   cd ..
   echo "export PATH=$RUBY_INSTALLATION_PATH/bin:"'$PATH' > /etc/profile.d/ruby.sh
   source /etc/profile.d/ruby.sh
-  # gem sources --add https://gems.ruby-china.org/ --remove https://rubygems.org/
+  # gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/
   # gem update --system
   # gem install rails
-  # bundle config mirror.https://rubygems.org https://gems.ruby-china.org
+  # bundle config mirror.https://rubygems.org https://gems.ruby-china.com
   set +x
 }
